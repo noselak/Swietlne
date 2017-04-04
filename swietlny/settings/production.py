a30307fc3882f16601115d92112c1c5324f9759e
noselak <dworanowski.adrian@gmail.com>
2017-04-04 13:14:58 +0000

    09 - Added news app; added js, css static files

--- swietlny/settings/production.py
+++ swietlny/settings/production.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'news',
 ]
 
 MIDDLEWARE = [
@@ -65,7 +66,7 @@ ROOT_URLCONF = 'swietlny.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
