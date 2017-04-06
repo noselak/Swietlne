b288227cf36eab98da7ad4f1b245673c32d3aeb4
noselak <dworanowski.adrian@gmail.com>
2017-04-06 11:05:20 +0000

    14 - Heroku Static Files Fix

--- swietlny/settings/base.py
+++ swietlny/settings/base.py
@@ -135,6 +135,8 @@ STATICFILES_DIRS = (
 
 STATIC_ROOT = os.path.join(BASE_DIR, "live-static", "static-root")
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 MEDIA_URL = "/media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "live-static", "media-root")
--- swietlny/urls.py
+++ swietlny/urls.py
@@ -21,8 +21,3 @@ urlpatterns = [
     url(r'', include('news.urls', namespace="news")),
     url(r'^admin/', admin.site.urls),
 ]
-
-if not settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    )
