fb93f0aa3e7e15f66a0c68d04dd9b2bf37aaec1e
noselak <dworanowski.adrian@gmail.com>
2017-06-05 11:01:59 +0000

    41 - Added export csv feature to newsletter model

--- news/admin.py
+++ news/admin.py
@@ -1,6 +1,35 @@
 from django.contrib import admin
+from django.http import HttpResponse
 
 from .models import Join, Faq
 
-admin.site.register(Join)
+def export_csv(modeladmin, request, queryset):
+    import csv
+    from django.utils.encoding import smart_str
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename=mymodel.csv'
+    writer = csv.writer(response, csv.excel)
+    response.write(u'\ufeff'.encode('utf8'))
+    writer.writerow([
+        smart_str(u"ID"),
+        smart_str(u"E-Mail"),
+    ])
+    for obj in queryset:
+        writer.writerow([
+            smart_str(obj.pk),
+            smart_str(obj.email),
+        ])
+    return response
+export_csv.short_description = u"Export CSV"
+
+
+class JoinAdmin(admin.ModelAdmin):
+    list_display = ["__str__"]
+    actions = [export_csv]
+    
+    class Meta:
+        model = Join
+
+
+admin.site.register(Join, JoinAdmin)
 admin.site.register(Faq)
